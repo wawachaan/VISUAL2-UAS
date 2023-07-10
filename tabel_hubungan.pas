d30e7df25e06c965326733a20e05245817c6008c
wawachaan <najwanorizat4@gmail.com>
2023-07-10 23:57:15 +0700

    menambahkan event onshow pada form hubungan

--- tabel_hubungan.pas
+++ tabel_hubungan.pas
@@ -38,6 +38,7 @@ type
     procedure DBGrid1CellClick(Column: TColumn);
     procedure btn4Click(Sender: TObject);
     procedure btn5Click(Sender: TObject);
+    procedure FormShow(Sender: TObject);
   private
     { Private declarations }
   public
@@ -196,6 +197,11 @@ procedure TForm7.btn5Click(Sender: TObject);
 begin
 posisiawal;
 bersih;
+end;
+
+procedure TForm7.FormShow(Sender: TObject);
+begin
+posisiawal;
 end;
 
 end.
